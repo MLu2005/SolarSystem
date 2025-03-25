38ca502df321b55d74dbcf20513da6ae877ca555
Grebe, Nick (Stud. DACS) <n.grebe@student.maastrichtuniversity.nl>
2025-03-25 11:40:16 +0100

    Overhauled and now working project for everyone

---
+++ src/main/java/executables/Main.java
@@ -0,0 +1,28 @@
+package executables;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/executables/HomeView.fxml"));
+
+        Scene scene = new Scene(loader.load());
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Solar System");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+
+        launch(args);
+
+    }
+}
+
