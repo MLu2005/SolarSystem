4a5ce92d193ba13c9d5d5c0562cd9468d43f3391
Martin <m.byczkowski@student.maastrichtuniversity.nl>
2025-05-04 20:10:18 +0200

    Created a Euclidean distance calculator from the previously created 3D vectors to calculate the shortest distance between celestial objects.

--- src/main/java/com/example/spaceMissions/D2O.java
+++ src/main/java/com/example/spaceMissions/D2O.java
@@ -1,10 +1,13 @@
 package com.example.spaceMissions;
 
 import com.example.solarSystem.Vector3D;
+import org.jetbrains.annotations.NotNull;
+
 import java.lang.Math;
 
 public class D2O {
 
+    private String name;
     private Vector3D object1;
     private Vector3D object2;
 
@@ -28,7 +31,16 @@ public class D2O {
         this.object2 = object2;
     }
 
-    public double getDistance(Vector3D object1, Vector3D object2) {
+    /**
+     *this method takes the 3D vector of 2 celestial bodies as parameters and extracts the
+     x, y, z coordinates of the 2 bodies and calculates the exact Euclidean distance using the Pythagoras rule
+
+     @param object1 any planet, moon, or star
+     @param object2 any other planet, moon, or star
+
+     */
+
+    public double getDistance(@NotNull Vector3D object1, @NotNull Vector3D object2) {
 
         double xDistance = Math.abs(object1.x - object2.x);
         double yDistance = Math.abs(object1.y - object2.y);
