29077267ce2df6a9aa3844808ea4d25568c5e99d
Martin <m.byczkowski@student.maastrichtuniversity.nl>
2025-05-06 01:08:09 +0200

    Added class SpaceShip, and deleted D2O

---
+++ src/main/java/com/example/spaceMissions/SpaceShip.java
@@ -0,0 +1,13 @@
+package com.example.spaceMissions;
+
+import com.example.solarSystem.Vector3D;
+
+public class SpaceShip {
+
+    private String name;
+    private double thrust;
+    private double velocity;
+    private double mass;
+    private double fuel;
+    private Vector3D position;
+}
