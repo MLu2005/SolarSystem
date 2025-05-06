2127e7e7ba9580a20d416eaf786c54cea9737cfb
Grebe, Nick (Stud. DACS) <n.grebe@student.maastrichtuniversity.nl>
2025-05-06 13:12:09 +0200

    fixed rk4

--- src/main/java/executables/solvers/RK4Solver.java
+++ src/main/java/executables/solvers/RK4Solver.java
@@ -3,6 +3,8 @@ package executables.solvers;
 import java.util.Arrays;
 import java.util.function.BiFunction;
 
+import static com.example.demo.ODEUtility.addVectors;
+import static com.example.demo.ODEUtility.scaleVector;
 import static executables.solvers.ODEUtility.*;
 
 public class RK4Solver implements ODESolver {
@@ -42,6 +44,25 @@ public class RK4Solver implements ODESolver {
 
         return values;
     }
+
+    public double[] solveStep(
+            BiFunction<Double, double[], double[]> f,
+            double t,
+            double[] y,
+            double stepSize
+    ) {
+        double[] k1 = f.apply(t, y);
+        double[] k2 = f.apply(t + stepSize / 2.0, addVectors(y, scaleVector(k1, stepSize / 2.0)));
+        double[] k3 = f.apply(t + stepSize / 2.0, addVectors(y, scaleVector(k2, stepSize / 2.0)));
+        double[] k4 = f.apply(t + stepSize, addVectors(y, scaleVector(k3, stepSize)));
+
+        double[] nextY = new double[y.length];
+        for (int j = 0; j < y.length; j++) {
+            nextY[j] = y[j] + (stepSize / 6.0) * (k1[j] + 2 * k2[j] + 2 * k3[j] + k4[j]);
+        }
+        return nextY;
+    }
+
 
 
 }
