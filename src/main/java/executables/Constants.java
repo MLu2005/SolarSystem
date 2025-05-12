ef948966bab632aae960a318b4d2f4f4834dde54
Martin <m.byczkowski@student.maastrichtuniversity.nl>
2025-05-12 17:27:54 +0200

    added a constant

--- src/main/java/executables/Constants.java
+++ src/main/java/executables/Constants.java
@@ -1,10 +1,10 @@
 package executables;
 
-
 public class Constants {
 
     public static final double TOLERANCE = 1e-9;
     public static final double EARTH_RADIUS_KM = 6371;
+    public static final double TITAN_RADIUS_KM = 2575;
     public static final double PROBE_MASS = 50000.0;
     public static final double  G = 6.6743E-20;
     public static final double SIM_LEN  = 365 * 86400;
